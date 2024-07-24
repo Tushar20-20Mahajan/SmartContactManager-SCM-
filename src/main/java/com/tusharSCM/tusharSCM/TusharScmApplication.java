ac63948e627937d12ed7b9daa680b095cf81e852
Tushar Mahajan <tusharmahajan080@gmail.com>
2024-07-24 12:05:40 +0530

    Starter file i.e. Spring Boot initializer file added

---
+++ src/main/java/com/tusharSCM/tusharSCM/TusharScmApplication.java
@@ -0,0 +1,13 @@
+package com.tusharSCM.tusharSCM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TusharScmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TusharScmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tusharSCM/tusharSCM/TusharScmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tusharSCM.tusharSCM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TusharScmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
