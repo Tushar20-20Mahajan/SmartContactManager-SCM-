12836b323ee6deadcd21f1b94e494881bdf2c990
Tushar Mahajan <tusharmahajan080@gmail.com>
2024-08-04 03:20:44 +0530

    Creating Admin Dashboard completed and side bar added and configured the pages for the small screen

--- src/main/java/com/tusharSCM/tusharSCM/controllers/UserControllers.java
+++ src/main/java/com/tusharSCM/tusharSCM/controllers/UserControllers.java
@@ -1,16 +1,10 @@
 package com.tusharSCM.tusharSCM.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.tusharSCM.tusharSCM.entities.User;
-import com.tusharSCM.tusharSCM.helpers.Helper;
-import com.tusharSCM.tusharSCM.services.UserService;
 
 @Controller
 @RequestMapping("/user")
